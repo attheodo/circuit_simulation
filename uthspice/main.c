b1752dc969ea969fde280f240da02cc13fb599c2
Thanos Theodoridis <at@atworks.gr>
2012-10-04 16:14:13 +0300

    Initial Commit

---
+++ uthspice/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  uthspice
+//
+//  Created by Athanasios Theodoridis on 4/10/12.
+//  Copyright (c) 2012 @attheodo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
