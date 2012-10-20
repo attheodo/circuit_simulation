cc6c65c76e18481d8f899136148c8628e7d0b661
Thanos Theodoridis <at@atworks.gr>
2012-10-20 16:07:34 +0300

    created separate files to hold types and models

---
+++ uthspice/types.h
@@ -0,0 +1,50 @@
+//
+//  types.h
+//
+//  Created by Athanasios Theodoridis (461) and Aris Eleftheriadis (460) on 4/10/12.
+//
+//
+
+// data structures and type definitions
+typedef struct element {
+    
+	char *element_name;
+	int element_type;
+    char *first_terminal;
+	char *second_terminal;
+    
+    char *drain_terminal;
+    char *gate_terminal;
+    char *source_terminal;
+    char *bulk_terminal;
+	
+    char *model_name;
+    
+    double gate_length;
+    double gate_width;
+	
+    char *area;
+	
+    double value;
+    
+	struct element *next;
+    
+} element;
+
+// simple enum for holding the type of circuit elements
+// we might parse
+typedef enum {
+    
+    elementTypeVoltageSource = 0,
+    elementTypeCurrentSource,
+    elementTypeResistance,
+    elementTypeCapacitor,
+    elementTypeInductor,
+    elementTypeDiode,
+    elementTypeMOSTransistor,
+    elementTypeBJTTransistor
+    
+} element_type;
+
+// simple BOOL typedef
+typedef enum { false, true } bool;
